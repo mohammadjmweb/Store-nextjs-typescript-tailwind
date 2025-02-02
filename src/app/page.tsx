266bcf77e6ddcb6a88f5f3b97adba5473c14cd84
mohammadjmweb <mohammadjmweb@gmail.com>
2025-02-02 06:07:31 +0330

    create page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,82 @@
-import Image from "next/image";
+"use client"
+import React,{useEffect,useState} from "react";
+import axios from "axios";
+import ProductCard from './components/ProductCard'
+import { useCart } from "./context/CartContext";
+import './App.css'
 
-export default function Home() {
+interface Product{
+  id:number;
+  name:string;
+  price:number;
+  img:string;
+}
+
+const Home:React.FC=()=> {
+  const [products,setProducts]=useState<Product[]>([])
+  const [isOpen,setIsOpen]=useState(false)
+
+  const toggleCart=()=>{
+    setIsOpen(!isOpen)
+  }
+
+  useEffect(()=>{
+    const fetchProducts=async()=>{
+      const response=await axios.get('http://localhost:5000/products')
+      setProducts(response.data)
+    }
+    fetchProducts()
+  },[])
+
+  const {cart,increaseQuantity,decreaseQuantity,removeFromCart,clearCart}=useCart()
+
+  const totalPrice=cart.reduce((total,item)=> total + item.price * item.quantity,0)
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="container mx-auto p-4">
+      <button onClick={toggleCart} className="mb-4 bg-green-500 text-white px-4 py-2 rounded">
+        {isOpen ? 'Close Cart' : 'View Cart'}
+      </button>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className={`rounded-xl fixed top-15 right-[5%] bg-blue-100 shadow-xl w-[90%] px-3 cart ${isOpen ? 'open' : 'close'}`}>
+            <h2 className=" mt-2 text-lg font-bold text-center">Shopping Cart</h2>
+            {cart.length === 0 ? (
+                <p>Shopping Cart is empty</p>
+            ) : (
+                <React.Fragment>
+                    <ul>
+                        {cart.map(item=>(
+                            <li key={item.id} className="flex justify-between items-center border-2 border-blue-950 my-2 py-1 px-2 rounded-lg">
+                                <span className="w-[30%]" >{item.name} </span>
+                                <span>(${item.price.toFixed(2)})</span>
+                                <div className="flex w-[20%] md:w-[10%] justify-between border-2 border-blue-950 rounded-lg px-2 " >
+                                    <button onClick={()=>decreaseQuantity(item.id)}>-</button>
+                                    <h3>{item.quantity}</h3>
+                                    <button onClick={()=>increaseQuantity(item.id)}>+</button>
+                                </div>
+                                <button className="text-red-500" onClick={()=>removeFromCart(item.id)}>remove</button>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="mt-4 flex justify-between items-center ">
+                        <strong>Total price: ${totalPrice.toFixed(2)}</strong>
+                        <button onClick={clearCart} className="mt-2 bg-red-500 text-white px-4 py-2 rounded">Clear Carts</button>
+                    </div>
+                    
+                </React.Fragment>
+            )}
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4" >
+        {products.map(product=>(
+          <ProductCard 
+            key={product.id} 
+            product={product}
+          /> 
+        ))}
+      </div>
+    </div>    
   );
 }
+
+export default Home
